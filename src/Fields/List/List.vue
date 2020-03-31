bc82af926c8c160b55780fe09bb63008068f64f7
Mykolas Mankevcius <mykolas@unity3d.com>
2020-03-31 21:50:25 +0300

    try again

--- src/Fields/List/List.vue
+++ src/Fields/List/List.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <input v-if="value.length === 0" type="text" :name="`${key}[]`" hidden />
+    <input
+      v-if="value.length === 0"
+      type="text"
+      :name="key"
+      :value="null"
+      hidden
+    />
     <input
       v-else
       v-for="val in value"
