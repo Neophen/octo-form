ec621456d339c6f95be8421d5d966f466183f4f9
Mykolas Mankevcius <mykolas@unity3d.com>
2020-09-26 00:38:34 +0300

    Refactor form

--- src/Form/Form.vue
+++ src/Form/Form.vue
@@ -1,47 +1,46 @@
 <template>
-  <validation-observer ref="refValidation" v-slot="{ handleSubmit }">
+  <ValidationObserver ref="refValidation" v-slot="{ handleSubmit }">
     <form ref="refForm" @submit.prevent="handleSubmit(onSubmit)">
-      <octo-form-field
-        v-for="(field, key) in newfieldset"
+      <OctoFormField
+        v-for="(field, key) in allfields"
         :class="fieldGap"
         :key="key"
         :field="field"
-        :fields="newfieldset"
+        :fields="allfields"
         :disabled="loading"
         v-model="field.value"
       />
       <div class="octo-form__buttons">
-        <o-button
-          v-if="formState.type === 'secondary'"
-          :type="formState.buttonType"
+        <OCheckbox
+          v-if="buttons === FORM_BUTTONS.CANCEL_ACCEPT"
+          v-model="accepted"
           class="octo-form__cancel-btn"
-          :dusk="formConfig.secondaryDusk"
-          :disabled="loading"
-          @click="formState.secondaryAction"
-        >
-          {{ formConfig.secondary }}
-        </o-button>
-        <o-checkbox
-          v-if="formState.type === 'accept'"
-          v-model="acceptTermsConditions"
-          class="octo-form__cancel-btn"
-          :name="formConfig.acceptName"
+          :name="form.accept_field_name"
         >
           <slot name="accept" />
-        </o-checkbox>
-        <o-button
+        </OCheckbox>
+        <OButton
+          v-else-if="buttons !== FORM_BUTTONS.CONFIRM_ONLY"
+          :type="cancelBtnStyle"
+          class="octo-form__cancel-btn"
+          :disabled="loading"
+          @click="cancelAction"
+        >
+          {{ form.secondary }}
+        </OButton>
+        <OButton
           native-type="submit"
           type="is-primary"
           class="octo-form__confirm-btn"
-          :dusk="formConfig.confirmDusk"
+          v-if="buttons !== FORM_BUTTONS.CANCEL_ONLY"
           :loading="loading"
           :disabled="!canSubmit"
         >
-          {{ formConfig.confirm }}
-        </o-button>
+          {{ form.confirm }}
+        </OButton>
       </div>
     </form>
-  </validation-observer>
+  </ValidationObserver>
 </template>
 
 <script>
@@ -51,41 +50,56 @@ export default {
   props: {
     fieldset: {
       type: Object,
-      required: true
+      required: true,
     },
     config: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     errors: [Array, Object],
-    loading: Boolean
+    loading: Boolean,
   },
   setup(props, { emit, root }) {
     const refForm = ref(null);
     const refValidation = ref(null);
 
+    // external
+    const FORM_BUTTONS = {
+      BOTH: "BOTH",
+      CANCEL_LINK: "CANCEL_LINK",
+      CANCEL_ACCEPT: "CANCEL_ACCEPT",
+      CONFIRM_ONLY: "CONFIRM_ONLY",
+      CANCEL_ONLY: "CANCEL_ONLY",
+    };
+
     const defaultConfig = {
       confirm: "Confirm",
-      confirmDusk: "octo-form__confirm-btn",
-      secondary: "Cancel",
-      secondaryDusk: "octo-form__cancel-btn",
-      type: "default",
+      cancel: "Cancel",
+      buttons: FORM_BUTTONS.BOTH,
       spacing: "octo-form__fields-gap",
-      secondaryAction: () => {},
-      acceptName: null
+      cancelAction: () => {},
+      accept_field_name: null,
     };
 
     const state = reactive({
-      newfieldset: props.fieldset,
-      acceptTermsConditions: false,
-      formConfig: { ...defaultConfig, ...props.config }
-    });
-
-    const canSubmit = computed(() => {
-      if (formState.value.type === "accept") {
-        return state.acceptTermsConditions;
-      }
-      return true;
+      allfields: props.fieldset,
+      accepted: false,
+      form: { ...defaultConfig, ...props.config },
+      buttons: computed(() => state.form.buttons),
+      cancelBtnStyle: computed(() => {
+        switch (state.buttons) {
+          case FORM_BUTTONS.CANCEL_LINK:
+            return "is-link";
+          default:
+            return "is-default";
+        }
+      }),
+      fieldGap: computed(() => state.form.spacing),
+      canSubmit: computed(() =>
+        state.form.buttons === FORM_BUTTONS.CANCEL_ACCEPT
+          ? state.accepted
+          : true,
+      ),
     });
 
     const isEmpty = value =>
@@ -101,21 +115,15 @@ export default {
           refValidation.value.setErrors(errors);
         });
       },
-      { immediate: true }
+      { immediate: true },
     );
 
     const onSubmit = async () => {
-      emit("submit-state", "start");
-
       const prepareFields = Object.values(refValidation.value.refs)
         .filter(field => field.$children[0] && field.$children[0].prepare)
         .map(field => field.$children[0]?.prepare());
 
-      emit("submit-state", "progress-25");
       const blobs = await Promise.all(prepareFields);
-
-      emit("submit-state", "progress-75");
-
       const formData = new FormData(refForm.value);
 
       blobs
@@ -129,13 +137,14 @@ export default {
           }
         });
 
-      emit("submit-state", "finished");
       emit("submit", formData);
     };
 
-    const fieldGap = computed(() => state.formConfig.spacing);
+    const cancelAction = () => {
+      if (state.buttons === FORM_BUTTONS.CANCEL_ACCEPT) {
+        return;
+      }
 
-    const secondaryAction = () => {
       let isDirty = false;
 
       if (refValidation.value) {
@@ -144,42 +153,17 @@ export default {
       }
 
       emit("cancel", isDirty);
-      state.formConfig.secondaryAction();
+      state.form.cancelAction();
     };
-
-    const formStates = {
-      default: {
-        buttonType: "is-default",
-        type: "secondary",
-        secondaryAction: secondaryAction
-      },
-      link: {
-        buttonType: "is-link",
-        type: "secondary",
-        secondaryAction: secondaryAction
-      },
-      accept: {
-        buttonType: "is-default",
-        type: "accept",
-        secondaryAction: () => {}
-      },
-      "no-cancel": {
-        type: "no-cancel"
-      }
-    };
-
-    const formState = computed(() => formStates[state.formConfig.type]);
 
     return {
       ...toRefs(state),
       refValidation,
-      formState,
       refForm,
       onSubmit,
-      canSubmit,
-      fieldGap,
-      secondaryAction
+      cancelAction,
+      FORM_BUTTONS,
     };
-  }
+  },
 };
 </script>
